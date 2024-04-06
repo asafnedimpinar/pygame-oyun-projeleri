ddd131e600b9d7473f154dc93f494cc6d884dc19
Asaf Nedim Pınar <94077369+147399@users.noreply.github.com>
2024-04-06 20:30:31 +0300

    Update 2 ping pong oyunu.py

--- 2 ping pong oyunu.py
+++ 2 ping pong oyunu.py
@@ -1,4 +1,8 @@
 import turtle
+import pygame
+
+# Pygame başlatma
+pygame.init()
 
 # Turtle ekranını oluşturma
 pencere = turtle.Screen()
@@ -37,35 +41,37 @@ ball.dy = 0.15  # Y eksenindeki hareket hızı
 yazi = turtle.Turtle()
 yazi.speed(0)
 yazi.color("white")
-yazi.goto(0,260)
-yazi.write("Oyuncu A : 0    Oyuncu B: 0",align="center",font=("Courier",24,"bold"))
+yazi.goto(0, 260)
+yazi.write("Oyuncu A: 0    Oyuncu B: 0", align="center", font=("Courier", 24, "bold"))
 yazi.hideturtle()
-puan_a =0
+puan_a = 0
 puan_b = 0
 
+# Ses dosyasını yükleme
+pygame.mixer.music.load("plop.mp3")
 
 # Sol raketin yukarı hareketi
 def raket_a_up():
     y = raket_a.ycor()
-    y = y + 20
+    y += 20
     raket_a.sety(y)
 
 # Sol raketin aşağı hareketi
 def raket_a_down():
     y = raket_a.ycor()
-    y = y - 20
+    y -= 20
     raket_a.sety(y)
 
 # Sağ raketin yukarı hareketi
 def raket_b_up():
     y = raket_b.ycor()
-    y = y + 20
+    y += 20
     raket_b.sety(y)
 
 # Sağ raketin aşağı hareketi
 def raket_b_down():
     y = raket_b.ycor()
-    y = y - 20
+    y -= 20
     raket_b.sety(y)
 
 # Tuş girişlerini dinleme
@@ -77,46 +83,47 @@ pencere.onkeypress(raket_b_down, "Down")
 
 # Oyun döngüsü
 while True:
-   
     pencere.update()  # Ekrani yenileme
 
     # Topun hareketini güncelleme
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
-    
 
     # Topun sınırları kontrol etme ve yönünü değiştirme
     if ball.ycor() > 290 or ball.ycor() < -290:
-        ball.dy = ball.dy * -1
+        ball.dy *= -1
     
     # Topun sağ sınıra ulaşıp ulaşmadığını kontrol etme
     if ball.xcor() > 390:
+        pygame.mixer.music.play()
         ball.goto(0, 0)
-        ball.dx = ball.dx * -1
-        puan_a = puan_a +1
+        ball.dx *= -1
+        puan_a += 1
         yazi.clear()
-        yazi.write("Oyuncu A : {}    Oyuncu B: {}".format(puan_a,puan_b),align="center",font=("Courier",24,"bold"))
+        yazi.write("Oyuncu A: {}    Oyuncu B: {}".format(puan_a, puan_b), align="center", font=("Courier", 24, "bold"))
 
     # Topun sol sınıra ulaşıp ulaşmadığını kontrol etme
     if ball.xcor() < -390:
+        pygame.mixer.music.play()
         ball.goto(0, 0)
-        ball.dx = ball.dx * -1 
-        puan_b = puan_b +1
+        ball.dx *= -1 
+        puan_b += 1
         yazi.clear()
-        yazi.write("Oyuncu A : {}    Oyuncu B: {}".format(puan_a,puan_b),align="center",font=("Courier",24,"bold"))
+        yazi.write("Oyuncu A: {}    Oyuncu B: {}".format(puan_a, puan_b), align="center", font=("Courier", 24, "bold"))
 
     
     # Topun sağ raket ile temasını kontrol etme
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < raket_b.ycor() + 60 and ball.ycor() > raket_b.ycor() - 60):
+        pygame.mixer.music.play()
         ball.setx(340)
-        ball.dx = ball.dx * -1
+        ball.dx *= -1
 
     # Topun sol raket ile temasını kontrol etme
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < raket_a.ycor() + 60 and ball.ycor() > raket_a.ycor() - 60):
+        pygame.mixer.music.play()
         ball.setx(-340)
-        ball.dx = ball.dx * -1
+        ball.dx *= -1
 
 
 # Ana ekran döngüsü
 pencere.mainloop()
-
